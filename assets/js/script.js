9f35a8e1661bcb770c81f8323fe2bd72c0f94f25
omatography <omkarslam420@gmail.com>
2020-11-26 21:51:20 +0530

    Many changes haha

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,85 @@ $(document).ready(() =>{
     $('#navbar').addClass(" animate__animated animate__fadeInDown animate__fast");
     $('#home-content').addClass(" animate__animated animate__fadeIn animate__fast");
     
+    $('#service-box3').mouseenter(() => {
+        $('#service-box3').addClass("box-after");
+        $('#img3').addClass("img-after");
+       
+        setTimeout(() => {
+            
+            $('#head3').addClass("d-none");
+        },10);
+        setTimeout(() => {
+            
+            $('#para3').removeClass("d-none");
+        },10);
+
+    }).mouseleave(() => {
+        $('#service-box3').removeClass("box-after");
+        $('#img3').removeClass("img-after");
+        $('#para3').addClass("d-none");
+        $('#head3').removeClass("d-none");
+        
+        
+
+    });
+
+    $('#service-box1').mouseenter(() => {
+        $('#service-box1').addClass("box-after");
+        $('#img1').addClass("img-after");
+       
+        setTimeout(() => {
+            
+            $('#head1').addClass("d-none");
+        },10);
+        setTimeout(() => {
+            
+            $('#para1').removeClass("d-none");
+        },10);
+
+    }).mouseleave(() => {
+        $('#service-box1').removeClass("box-after");
+        $('#img1').removeClass("img-after");
+        $('#para1').addClass("d-none");
+        $('#head1').removeClass("d-none");
+        
+        
+
+    });
+
+    $('#service-box2').mouseenter(() => {
+        $('#service-box2').addClass("box-after");
+        $('#img2').addClass("img-after");
+       
+        setTimeout(() => {
+            
+            $('#head2').addClass("d-none");
+        },10);
+        setTimeout(() => {
+            
+            $('#para2').removeClass("d-none");
+        },10);
+
+    }).mouseleave(() => {
+        $('#service-box2').removeClass("box-after");
+        $('#img2').removeClass("img-after");
+        $('#para2').addClass("d-none");
+        $('#head2').removeClass("d-none");
+        
+        
+
+    });
+    // $('#service-box').mouseleave(() => {
+    //     $('#service-box').removeClass("box-after");
+    //     $('#img').removeClass("img-after");
+    //     $('#head').removeClass("d-none");
+    //     $('#para').addClass("d-none");
+
+    // });
+
+
+
+
 
     $('#about').waypoint(function(direction){
         $('#about').addClass('animate__animated animate__fadeIn animate__fast ');
